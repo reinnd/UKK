3c7d125b92fdf996c6152c4e9dfcd8276e252ca1
renn.7z <161840172+reinnd@users.noreply.github.com>
2026-01-14 16:02:27 +0700

    nwe

---
+++ UKK_3_Putra/view/login.php
@@ -0,0 +1,36 @@
+<?php ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="style/login_style.css?v=1.1">
+</head>
+<body>
+    <main>
+        <div class="box">
+            <div class="box2"><br>
+            <p style="font-size: 40px;">login</p>
+
+            <label for="username">Username</label>
+            <br>
+            <input type="text">
+            <br><br>
+
+            <label for="password">Password</label>
+            <br>
+            <input type="password">
+            <br><br>
+
+            <button>login</button>
+        </div>
+            
+            <div class="bottom_text">
+                <p class="reg">blahhhhhhhh <a href="">Register</a> blahhh!</p>
+            </div>
+        </div>
+    </main>
+</body>
+</html>
