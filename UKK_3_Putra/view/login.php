f8a61e0fc748b0fdff6a26e76a6c0ebe6a3fb573
renn.7z <161840172+reinnd@users.noreply.github.com>
2026-01-19 16:01:52 +0700

    new css
    new css

--- UKK_3_Putra/view/login.php
+++ UKK_3_Putra/view/login.php
@@ -6,31 +6,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="style/login_style.css?v=1.1">
+    <link rel="stylesheet" href="style/login_style.css?v=1.2">
 </head>
 <body>
     <main>
         <div class="box">
             <div class="box2"><br>
-            <p style="font-size: 40px;">login</p>
-
-            <label for="username">Username</label>
-            <br>
-            <input type="text">
-            <br><br>
-
-            <label for="password">Password</label>
-            <br>
-            <input type="password">
-            <br><br>
-
-            <button>login</button>
-        </div>
-            
-            <div class="bottom_text">
+                <p style="font-size: 40px;">login</p>
+                
+                <form action="">
+                    <fieldset>
+                        <label for=""></label>
+                        <input type="text">
+                        <label for=""></label>
+                        <input type="password">
+                    </fieldset>
+                    <button class="login" type="submit">login</button>
                 <p class="reg">blahhhhhhhh <a href="">Register</a> blahhh!</p>
+                <span><button>regist</button></span>
+                </form>
             </div>
         </div>
+
+            
     </main>
 </body>
 </html>
